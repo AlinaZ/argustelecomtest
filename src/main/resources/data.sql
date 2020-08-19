7fb49155807a9f7d220141afc699158dd8151ea7
Загитова Алина Ильгизовна <Zagitova.A.I@sberbank.ru>
2020-08-20 00:19:27 +0500

    Добавлено заполнение базы данных демоданными

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,3 +17,9 @@ INSERT INTO Point (point_id,version,point_cu_id,cu_id) VALUES (6,0,2,3);
 INSERT INTO Point (point_id,version,point_cu_id,cu_id) VALUES (7,0,3,3);
 INSERT INTO Point (point_id,version,point_cu_id,cu_id) VALUES (8,0,1,4);
 INSERT INTO Point (point_id,version,point_cu_id,cu_id) VALUES (9,0,1,5);
+
+INSERT INTO Link (link_id,version,point1_id,point2_id) VALUES (1,0,1,7);
+INSERT INTO Link (link_id,version,point1_id,point2_id) VALUES (1,0,2,7);
+INSERT INTO Link (link_id,version,point1_id,point2_id) VALUES (1,0,8,9);
+INSERT INTO Link (link_id,version,point1_id,point2_id) VALUES (1,0,8,5);
+INSERT INTO Link (link_id,version,point1_id,point2_id) VALUES (1,0,3,7);
